59783dc56a3704f3311fc68ad59fa7c0231ebb32
Michigan22 <63482443+Michigan22@users.noreply.github.com>
2022-03-12 21:28:47 -0600

    Add files via upload

---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div class="topnav">
+  <div class="topnav-centered">
+    <a href="#home" class="active">Home</a>
+  </div>
+  <a href="#stuff">Stuff</a>
+  <a href="#morestuff">More Stuff</a>
+  <div class="topnav-right">
+    <a href="#otherstuff">Other Stuff</a>
+    <a href="#less">Less Stuff</a>
+  </div>
+  
+</div>
+    )
+}
